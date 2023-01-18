7af7844f5c3cf5117b40c1364920bd6e1e5b360e
Justin <neophilia85@gmail.com>
2023-01-18 13:13:37 +0100

    Header search layout and click event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Basket
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -11,4 +12,23 @@ basketEl.addEventListener("click", (e) => {
 
 window.addEventListener("click", () => {
     basketEl.classList.remove("show");
+
 })
+
+//Search
+const header = document.querySelector("header");
+const searchIcon = header.querySelector(".search-starter");
+const searchClose = header.querySelector(".search-wrap .search-close");
+const shadowEl = header.querySelector(".search-wrap .shadow");
+const showSearch = () => {
+    header.classList.add("searching");
+    document.documentElement.classList.add("fixed");
+}
+const hideSearch = () => {
+    header.classList.remove("searching");
+    document.documentElement.classList.remove("fixed");
+}
+
+searchIcon.addEventListener("click", showSearch);
+searchClose.addEventListener("click", hideSearch);
+shadowEl.addEventListener("click", hideSearch);
