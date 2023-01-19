a02e8cdecf4e9f8ae339ace689678fd7af819141
Justin <neophilia85@gmail.com>
2023-01-19 09:47:32 +0100

    Search and Header Animation

--- js/main.js
+++ js/main.js
@@ -20,13 +20,27 @@ const header = document.querySelector("header");
 const searchIcon = header.querySelector(".search-starter");
 const searchClose = header.querySelector(".search-wrap .search-close");
 const shadowEl = header.querySelector(".search-wrap .shadow");
+const inputEl = header.querySelector(".search-wrap input");
+const headerLists = [...header.querySelectorAll(".menu>li")];
+
+const disappearLists = () => {
+    headerLists.reverse().map((list, index) => {
+        list.style.transitionDelay = index * .4 / headerLists.length + "s";
+    })
+}
 const showSearch = () => {
     header.classList.add("searching");
     document.documentElement.classList.add("fixed");
+    disappearLists();
+    setTimeout(() => {
+        inputEl.focus();
+    }, 600)
 }
 const hideSearch = () => {
     header.classList.remove("searching");
     document.documentElement.classList.remove("fixed");
+    disappearLists();
+    inputEl.value = "";
 }
 
 searchIcon.addEventListener("click", showSearch);
